9ee863b0f37f060b95089add156846a365818e12
xicocana <kanana.traceur@gmail.com>
2019-11-08 01:57:52 +0000

    inicio app

---
+++ DriveKeeper/app/src/main/java/meic/sirs/drivekeeper/LoginActivity.java
@@ -0,0 +1,26 @@
+package meic.sirs.drivekeeper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button login = (Button) findViewById(R.id.btn_login);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, UserOptionsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
